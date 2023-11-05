7a543aa968eb7ea12766849db96de868c9b024cd
Alisson Castro <t719132@f1rst.com.br>
2023-11-05 15:08:14 -0300

    ajuste actions terraform

--- version.tf
+++ version.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket  = "management-state-tf"
+    key     = "rds/terraform.tfstate"
+    region  = "sa-east-1"
+    profile = "fiap-env"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
